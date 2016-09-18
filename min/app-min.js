9a9104f07182084ff90921faa534a5b8fea4c351
FreelancerAsul <asul5000@gmail.com>
2016-09-18 00:29:15 -0400

    fixed port

--- min/app-min.js
+++ min/app-min.js
@@ -1,1 +1,1 @@
-var express=require("express"),app=express();app.use(express["static"](__dirname+"/public")),app.listen(8e3);
+var express=require("express"),app=express();app.use(express["static"](__dirname+"/public"));var port=process.env.PORT||8e3;app.listen(port);
